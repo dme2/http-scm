b7e803bc14b219035c1bd60372910651bad236ec
notdave22 <dme2223@gmail.com>
2020-05-28 10:16:33 -0500

    working on response

--- httpserv.scm
+++ httpserv.scm
@@ -4,10 +4,12 @@
 (import (chicken io))
 (require-extension srfi-13)
 
-(define _200_ "HTTP/1.0 200 OK\r\n")
-(define _404_ "HTTP/1.0 404 Not Found\r\n\notfound\n")
-(define _405_ "HTTP/1.0 405 Method Not Allowed \r\n\r\not allowed\n")
-(define port 2020)
+(define _200_ "HTTP/1.1 200 OK\r\n")
+(define _404_ "HTTP/1.1 404 Not Found\r\n\notfound\n")
+(define _405_ "HTTP/1.1 405 Method Not Allowed \r\n\r\not allowed\n")
+(define port 8000)
+
+;;needs to send a file (using connected-socket(file-descriptor)) instead of just plain text - although that works for now.
 
 ;; GET -PATH -VERSION
 ;;return "route".html
@@ -15,7 +17,9 @@
   (printf "processing\n")
   ;;get the path and check that it's valid
   (let* ((path (parse-req-path s))
-         (route (car(string-split path "/")))
+         (route (if (= 1 (string-length path))
+                    "index"
+                    (car(string-split path "/"))))
 	 (file (conc route ".html"))
 	 ;(ctype ())
 	 ;(clen  ())
@@ -30,6 +34,9 @@
    ((substring=? s "GET" 0 0 3) (process-get s))
     (else _404_)))
 
+;(define (set-header)
+  ;())
+
 (let ((sock (socket af/inet sock/stream))
       (backlog 1))
   (socket-bind sock (inet-address "127.0.0.1" port))
@@ -41,9 +48,14 @@
          (received-data (socket-receive connected-socket
                                         message-len)))
     ;;respond
+    ;;should do a filewrite here
     (printf "recvd:  ~A~%" received-data)
-    (let* ((resp (check_req received-data))) 
+    (let* ((content (check_req received-data))
+           (header (set-resp-header))
+           (resp (conc header content)))
       (printf resp)
+      ;(file-write connected-socket resp))
       (socket-send connected-socket resp))
+
     (socket-close connected-socket)
     (socket-close sock)))

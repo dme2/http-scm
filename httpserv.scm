b5b9d3f7db0df78d3b0c71c5a447b119053ac9a4
notdave22 <dme2223@gmail.com>
2020-06-03 12:29:48 -0500

    writes to the fd

--- httpserv.scm
+++ httpserv.scm
@@ -1,16 +1,28 @@
 (import socket)
 (import (chicken format))
+(import (chicken foreign))
 (import (chicken file posix))
 (import (chicken string))
 (import (chicken io))
+(import bind)
 (require-extension srfi-13)
 
+(bind "size_t strlen(const char *);")
 (define _200_ "HTTP/1.1 200 OK\r\n")
 (define _404_ "HTTP/1.1 404 Not Found\r\n\notfound\n")
 (define _405_ "HTTP/1.1 405 Method Not Allowed \r\n\r\not allowed\n")
 (define port 8000)
+(define content-type "Content-Type: text/html\r\n")
+(define content-length "Content-Length: % \r\n\r\n ")
 
 ;;needs to send a file (using connected-socket(file-descriptor)) instead of just plain text - although that works for now.
+
+
+(define (get-header len)
+  (let* ((s-len (number->string len)))
+    (conc content-type
+         (string-translate*
+          content-length `(("%" . ,s-len )) ))))
 
 ;; GET -PATH -VERSION
 ;;return "route".html
@@ -25,7 +37,7 @@
 	 ;(ctype ())
 	 ;(clen  ())
 	 )
-    (string-append (string-join (read-lines(open-input-file file)))_200_)))
+    (string-join (read-lines(open-input-file file)))))
 
 (define (parse-req-path s)
   (car (cdr (string-split s))))
@@ -34,9 +46,6 @@
   (cond
    ((substring=? s "GET" 0 0 3) (process-get s))
     (else _404_)))
-
-;(define (set-header)
-  ;())
 
 (let ((sock (socket af/inet sock/stream))
       (backlog 1))
@@ -51,10 +60,10 @@
     ;;respond
     ;;should do a filewrite here
     (printf "recvd:  ~A~%" received-data)
-    (let* ((content (check_req received-data)))
-           ;(header (set-resp-header))
-           ;(resp (conc header content)))
-      (printf content)
+    (let* ((content (check_req received-data))
+           (header (get-header (strlen content)))
+           (resp (conc _200_ header content)))
+      ;(printf content)
       (file-write (socket-fileno connected-socket) content))
       ;(socket-send connected-socket resp))
 
